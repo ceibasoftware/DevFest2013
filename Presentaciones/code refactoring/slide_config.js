898816d17fe6de0721302ba72c77c96c4a384dfb
hmetaute <hernan.metaute@gmail.com>
2013-04-12 15:43:34 -0500

    Se sube por fin el super codigo de super nelson

--- Presentaciones/code refactoring/slide_config.js
+++ Presentaciones/code refactoring/slide_config.js
@@ -1,9 +1,9 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Drools Blip<br>Introducción a la plataforma',
-    subtitle: 'Expert, Guvnor, Fusion y jBPM5',
-    //eventTitle: 'Google I/O 2013',
+    title: 'Refactorización de código<br>',
+    subtitle: 'Un código eficiente es bueno pero un código claro es mejor',
+    eventTitle: 'Google I/O 2013',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
@@ -19,18 +19,11 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Andres Felipe Cano Amaya',
-    company: 'Gerente Técnico, Ceiba Software',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
-  },{
     name: 'Nelson Manuel Moreno Restrepo',
     company: 'Arquitecto, Ceiba Software',
     gplus: 'http://plus.google.com/1234567890',
     twitter: '@notesla',
-    www: 'http://www.you.com',
+    www: 'http://isomorfo.blogspot.com',
     github: 'http://github.com/nelsonmoreno'
   }]
 };
